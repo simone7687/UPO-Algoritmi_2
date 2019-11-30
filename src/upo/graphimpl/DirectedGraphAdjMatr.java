828fc47a5276d86235230b6718bff4951f3730f6
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2019-11-30 10:39:39 +0100

    removeVertex

--- src/upo/graphimpl/DirectedGraphAdjMatr.java
+++ src/upo/graphimpl/DirectedGraphAdjMatr.java
@@ -121,10 +121,40 @@ public class DirectedGraphAdjMatr implements Graph
 		return false;
 	}
 
+	/**
+	 * Removes the specified vertex from this graph including all its touching edges
+	 * if present. More formally, if the graph contains a vertex <code>
+	 * u</code> such that <code>u.equals(v)</code>, the call removes all edges that
+	 * touch <code>u</code> and then removes <code>u</code> itself. If no such
+	 * <code>u</code> is found, the call leaves the graph unchanged. Returns
+	 * <tt>true</tt> if the graph contained the specified vertex. (The graph will
+	 * not contain the specified vertex once the call returns).
+	 *
+	 * <p>
+	 * If the specified vertex is <code>null</code> returns <code>
+	 * false</code>.
+	 * </p>
+	 *
+	 * @param v
+	 *            vertex to be removed from this graph, if present.
+	 *
+	 * @return <code>true</code> if the graph contained the specified vertex;
+	 *         <code>false</code> otherwise.
+	 */
 	@Override
 	public boolean removeVertex(Vertex v)
 	{
-		// TODO Auto-generated method stub
+		if (v == null || !containsVertex(v))
+			return false;
+		for (int i=0; i<maxNVertex; i++)
+		{
+			if (graph[i][0] == v)
+			{
+				for (int k=0; k<maxNVertex; k++)
+					graph[i][k] = null;
+				return true;
+			}
+		}
 		return false;
 	}
 
