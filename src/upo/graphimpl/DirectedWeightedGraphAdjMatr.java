1052deba8929ed707c43407d9d3fdaaa0018d489
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2019-12-23 17:57:29 +0100

    DirectedWeightedGraphAdjMatr

--- src/upo/graphimpl/DirectedWeightedGraphAdjMatr.java
+++ src/upo/graphimpl/DirectedWeightedGraphAdjMatr.java
@@ -1,5 +1,106 @@
 package upo.graphimpl;
 
-public class DirectedWeightedGraphAdjMatr extends DirectedGraphAdjMatr {
+import upo.graph.*;
 
+/**
+ * TODO: javadoc
+ */
+public class DirectedWeightedGraphAdjMatr extends DirectedGraphAdjMatr implements Graph {
+    /**
+     * Creates a new edge in this graph, going from the source vertex to the target
+     * vertex, and returns the created edge. Some graphs do not allow
+     * edge-multiplicity. In such cases, if the graph already contains an edge from
+     * the specified source to the specified target, then this method does not
+     * change the graph and returns <code>null</code>.
+     *
+     * <p>
+     * The source and target vertices must already be contained in this graph. If
+     * they are not found in graph IllegalArgumentException is thrown.
+     * </p>
+     *
+     * @param sourceVertex source vertex of the edge.
+     * @param targetVertex target vertex of the edge.
+     * @param weight
+     * @return The newly created edge if added to the graph, otherwise <code>
+     * null</code>.
+     * @throws IllegalArgumentException if source or target vertices are not found in the graph.
+     * @throws NullPointerException     if any of the specified vertices is <code>
+     *                                  null</code>.
+     */
+    public Edge addEdge(Vertex sourceVertex, Vertex targetVertex, double weight) {
+        //If any of the specified vertex are null, throws NullPointerException
+        if (sourceVertex == null || targetVertex == null)
+            throw new NullPointerException("One or more given parameter is null.");
+
+        //If the graph doesn't contain both the specified vertex, throws IllegalArgumentException
+        if (!containsVertex(sourceVertex) || !containsVertex(targetVertex))
+            throw new IllegalArgumentException("One or both vertices are not contained in the graph.");
+
+        //Checks if the list doesn't contain the value already, creates an edge, adds it to the edges set and returns it
+        if (!containsEdge(sourceVertex, targetVertex))
+            for (int i = 1; i < maxNVertex; i++)
+                if (edgees[i] == null) {
+                    EdgeImpl e = new EdgeImpl(sourceVertex, targetVertex, weight, this);
+                    edgees[i] = e;
+                    return e;
+                }
+
+        return null;
+    }
+
+    /**
+     * Assigns a weight to an edge.
+     *
+     * @param sourceVertex source vertex of the edge.
+     * @param targetVertex target vertex of the edge.
+     * @param weight       new weight for edge
+     * @throws UnsupportedOperationException if the graph does not support weights
+     */
+    @Override
+    public void setEdgeWeight(Vertex sourceVertex, Vertex targetVertex, double weight) {
+        if (containsEdge(sourceVertex, targetVertex))
+            for (int i = 0; i < maxNVertex; i++)
+                if (edgees[i] != null)
+                    if (edgees[i].getSource().equals(sourceVertex) && edgees[i].getTarget().equals(targetVertex))
+                        edgees[i].setEdgeWeight(weight);
+    }
+
+    /**
+     * TODO: java doc
+     */
+    @Override
+    public GraphSearchResult visit(SearchType type) throws UnsupportedOperationException {
+        //If there's no nodes, returns null
+        if (graph == null)
+            return null;
+
+        GraphSearchResultImpl tree;
+
+        setNotVisitedNodes();
+
+        Vertex root = getHeadVertices().get(0);
+        tree = new GraphSearchResultImpl(type, root, this);
+
+        switch (type) {
+            case BFS:
+                throw new UnsupportedOperationException("This visit cannot be performed on unweighted graphs");
+            case DFS:
+                throw new UnsupportedOperationException("This visit cannot be performed on unweighted graphs");
+            case DFS_TOT:
+                throw new UnsupportedOperationException("This visit cannot be performed on unweighted graphs");
+            case DIJKSTRA:
+                return null;    //TODO:...
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * TODO: javdoc
+     */
+    public double[][] allToAllShortestPaths() {
+        double[][] paths = new double[maxNVertex][maxNVertex];
+        //TODO:...
+        return paths;
+    }
 }
---
+++ src/upo/graphtests/DirectedWeightedGraphAdjMatrTest.java
@@ -0,0 +1,403 @@
+package upo.graphtests;
+
+import org.junit.Test;
+import upo.graphimpl.DirectedWeightedGraphAdjMatr;
+import upo.graphimpl.VertexImpl;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DirectedWeightedGraphAdjMatrTest {
+    private DirectedWeightedGraphAdjMatr graph;
+
+    public DirectedWeightedGraphAdjMatrTest() {
+        graph = new DirectedWeightedGraphAdjMatr();
+    }
+
+    @Test
+    public void addVertexTest() {
+        //Tests exception
+        assertThrows(NullPointerException.class, () -> {
+            graph.addVertex(null);
+        });
+
+        assertEquals(graph.vertexSet().size(), 0);
+
+        //Add 3 vertices
+        VertexImpl v0 = new VertexImpl("A", graph);
+        boolean v0res = graph.addVertex(v0);
+        assertTrue(v0res);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        assertTrue(graph.addVertex(v1));
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        assertTrue(graph.addVertex(v2));
+
+        boolean v3res = graph.addVertex(v0);
+        assertFalse(v3res);
+
+        VertexImpl v4 = new VertexImpl("A", graph);
+        assertFalse(graph.addVertex(v4));
+
+        //Confirm that vertices have been added correctly
+        assertEquals(graph.vertexSet().size(), 3);
+    }
+
+    @Test
+    public void containsVertexTest() {
+        VertexImpl v2 = new VertexImpl("C", graph);
+        boolean v2res = graph.addVertex(v2);
+        assertTrue(v2res);
+
+        assertFalse(graph.containsVertex(null));
+        assertTrue(graph.containsVertex(v2));
+    }
+
+    @Test
+    public void removeVertexTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        assertEquals(3, graph.vertexSet().size());
+        assertFalse(graph.removeVertex(null));
+
+        boolean vertexnotexists = graph.removeVertex(new VertexImpl("F", graph));
+        assertFalse(vertexnotexists);
+
+        assertEquals(3, graph.vertexSet().size());
+        assertTrue(graph.removeVertex(v0));
+        assertEquals(2, graph.vertexSet().size());
+    }
+
+    @Test
+    public void vertexSetTest() {
+        graph.addVertex(new VertexImpl("A", graph));
+        graph.addVertex(new VertexImpl("B", graph));
+        graph.addVertex(new VertexImpl("C", graph));
+
+        assertEquals(3, graph.vertexSet().size());
+    }
+
+    @Test
+    public void addEdgeTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        assertThrows(NullPointerException.class, () -> {
+            graph.addEdge(null, null);
+        });
+
+        assertThrows(NullPointerException.class, () -> {
+            graph.addEdge(v0, null);
+        });
+
+        assertThrows(NullPointerException.class, () -> {
+            graph.addEdge(null, v0);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            graph.addEdge(new VertexImpl("D", graph), new VertexImpl("F", graph));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            graph.addEdge(v0, new VertexImpl("F", graph));
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            graph.addEdge(new VertexImpl("F", graph), v0);
+        });
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertEquals(1, graph.edgeSet().size());
+    }
+
+    @Test
+    public void containsEdgeTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        graph.addEdge(v0, v1);
+        assertFalse(graph.containsEdge(new VertexImpl("F", graph), v0));
+        assertFalse(graph.containsEdge(v0, new VertexImpl("F", graph)));
+
+        assertTrue(graph.containsEdge(v0, v1));
+        assertFalse(graph.containsEdge(v1, v0));
+    }
+
+    @Test
+    public void edgeSetTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        VertexImpl v3 = new VertexImpl("D", graph);
+        graph.addVertex(v3);
+
+        assertEquals(0, graph.edgeSet().size());
+
+        graph.addEdge(v0, v1);
+        graph.addEdge(v0, v2);
+        graph.addEdge(v0, v3);
+
+        assertEquals(3, graph.edgeSet().size());
+    }
+
+    @Test
+    public void degreeOfTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        assertEquals(0, graph.degreeOf(v0));
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertNotNull(graph.addEdge(v0, v2));
+
+        assertEquals(2, graph.degreeOf(v0));
+    }
+
+    @Test
+    public void inDegreeOfTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        assertEquals(0, graph.degreeOf(v0));
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertNotNull(graph.addEdge(v1, v0));
+        assertNotNull(graph.addEdge(v0, v2));
+
+        assertEquals(1, graph.inDegreeOf(v0));
+    }
+
+    @Test
+    public void outDegreeOf() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        assertEquals(0, graph.outDegreeOf(v0));
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertNotNull(graph.addEdge(v0, v2));
+
+        assertEquals(2, graph.outDegreeOf(v0));
+    }
+
+    @Test
+    public void removeEdgeTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertNotNull(graph.addEdge(v0, v2));
+        assertEquals(2, graph.edgeSet().size());
+
+        assertNull(graph.removeEdge(null, null));
+        assertNull(graph.removeEdge(v0, null));
+        assertNull(graph.removeEdge(null, v0));
+        assertNull(graph.removeEdge(new VertexImpl("F", graph), v0));
+        assertNull(graph.removeEdge(v0, new VertexImpl("F", graph)));
+    }
+
+    @Test
+    public void getEdgeWeightTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        assertNotNull(graph.addEdge(v0, v1));
+
+        assertEquals(1.0, graph.getEdgeWeight(v0, v1), 0);
+    }
+
+    @Test
+    public void setEdgeWeightTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        assertNotNull(graph.addEdge(v0, v1));
+
+        graph.setEdgeWeight(v0, v1, 2.0);
+
+        assertEquals(2.0, graph.getEdgeWeight(v0, v1), 0);
+    }
+
+    @Test
+    public void visitTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        VertexImpl v3 = new VertexImpl("D", graph);
+        graph.addVertex(v3);
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertNotNull(graph.addEdge(v0, v2));
+        assertNotNull(graph.addEdge(v1, v2));
+        assertNotNull(graph.addEdge(v1, v3));
+        assertNotNull(graph.addEdge(v2, v3));
+
+        //TODO:...
+    }
+
+    @Test
+    public void isCyclicTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertNotNull(graph.addEdge(v1, v2));
+
+        assertEquals(false, graph.isCyclic());
+
+        assertNotNull(graph.addEdge(v2, v0));
+
+        assertEquals(true, graph.isCyclic());
+    }
+
+    @Test
+    public void isDAGTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        VertexImpl v3 = new VertexImpl("D", graph);
+        graph.addVertex(v3);
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertNotNull(graph.addEdge(v1, v2));
+        assertNotNull(graph.addEdge(v2, v3));
+
+        assertEquals(false, graph.isCyclic());
+
+        assertNotNull(graph.addEdge(v2, v0));
+
+        assertEquals(true, graph.isCyclic());
+    }
+
+    @Test
+    public void topologicalSortTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        VertexImpl v3 = new VertexImpl("D", graph);
+        graph.addVertex(v3);
+
+        VertexImpl v4 = new VertexImpl("E", graph);
+        graph.addVertex(v4);
+
+        VertexImpl v5 = new VertexImpl("F", graph);
+        graph.addVertex(v5);
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertNotNull(graph.addEdge(v0, v2));
+        assertNotNull(graph.addEdge(v2, v3));
+        assertNotNull(graph.addEdge(v4, v0));
+        assertNotNull(graph.addEdge(v0, v5));
+
+        VertexImpl[] a = {v4, v0, v1, v2, v5, v3};
+
+        assertArrayEquals(a, graph.topologicalSort());
+    }
+
+    @Test
+    public void toStringSCCTest() {
+        VertexImpl v0 = new VertexImpl("A", graph);
+        graph.addVertex(v0);
+
+        VertexImpl v1 = new VertexImpl("B", graph);
+        graph.addVertex(v1);
+
+        VertexImpl v2 = new VertexImpl("C", graph);
+        graph.addVertex(v2);
+
+        VertexImpl v3 = new VertexImpl("D", graph);
+        graph.addVertex(v3);
+
+        VertexImpl v4 = new VertexImpl("E", graph);
+        graph.addVertex(v4);
+
+        assertNotNull(graph.addEdge(v0, v1));
+        assertNotNull(graph.addEdge(v1, v2));
+        assertNotNull(graph.addEdge(v2, v0));
+
+        assertNotNull(graph.addEdge(v1, v3));
+        assertNotNull(graph.addEdge(v3, v4));
+        assertNotNull(graph.addEdge(v3, v2));
+
+        assertNotNull(graph.stronglyConnectedComponents());
+
+        assertEquals("{{A,C,B},{D},{E}}", graph.toStringSCC());
+    }
+}
